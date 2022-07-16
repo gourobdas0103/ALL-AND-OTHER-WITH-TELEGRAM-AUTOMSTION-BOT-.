c5a43df16509ca289ed53ca84b65c6f17aaba922
GOUROB DAS <90569569+gourabdas0987@users.noreply.github.com>
2022-07-16 17:26:37 +0530

    Initialize college

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+'''
+ Author: gourab
+ Licenced to : ABC company
+*******hanks for reading*********
+'''
+import os# importing the os module
+print("hello world")
---
+++ even_or_odd.py
@@ -0,0 +1,7 @@
+'''find even or odd number'''
+
+x=int(input("enter a number"))
+if x/2==0:
+  print(x,"is even")
+else:
+    print(x,"is odd")
